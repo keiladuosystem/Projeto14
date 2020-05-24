7080a67f0390e2458498058727dc90cdc27ecee6
keiladuosystem <64993686+keiladuosystem@users.noreply.github.com>
2020-05-24 13:11:44 -0300

    Atualização do Driver
    Atualização do Driver

--- Triagem/src/test/java/triagem/Cadastro.java
+++ Triagem/src/test/java/triagem/Cadastro.java
@@ -142,7 +142,7 @@ public class Cadastro {
 		url1 = "https://coronavirusportugal.duodoctor.com.br";
 		String cwd = System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver",
-				cwd+"/driver/81/chromedriver.exe");
+				cwd+"/driver/83/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
@@ -160,7 +160,7 @@ public class Cadastro {
 
 	public void Print(String nomePrint) throws IOException {
 		File foto = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(foto, new File ( "\\\\192.168.111.123\\Jenkins-QA\\DuoDoctor14\\Evidencia"
+		FileUtils.copyFile(foto, new File ( "\\\\192.168.111.123\\Jenkins-QA\\DuoDoctor15\\Evidencia"
 				+ pastaPrint + cod +"\\" + nomePrint + ".png"));
 	}
 
@@ -311,7 +311,7 @@ public class Cadastro {
 		 driver.navigate (). refresh ();
 		 Thread.sleep(5000);
 		 driver.navigate (). refresh ();
-		 Thread.sleep(5000);
+		Thread.sleep(5000);
 		 driver.navigate (). refresh ();
 		 Thread.sleep(5000);
 		 driver.navigate (). refresh ();
